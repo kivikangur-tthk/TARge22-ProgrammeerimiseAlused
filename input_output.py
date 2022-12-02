82744341d6f02cd282ee787c0f212d73b87ac232
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2022-12-02 17:04:08 +0200

    Add todays python file

---
+++ input_output.py
@@ -0,0 +1,19 @@
+print("Tere\nkool!")
+
+print("""Tere\\
+kool!""")
+
+print("Tere","kool",2022,True,3.5,"Jah",end="\n",sep=" ")
+
+print("Tere"+"kool"+str(2022)+str(True)+str(3.5)+"Jah")
+
+
+nimi = input("Palun sisesta oma nimi: ")
+
+print("Tere " + nimi)
+print("Tere",nimi)
+print(f"Tere {nimi}")
+
+
+
+
